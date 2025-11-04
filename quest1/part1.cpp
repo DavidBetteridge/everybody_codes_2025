5091008c1f9bd98d4b93e5c76a764dd383f0a75e
davidbetteridge <david.betteridge@zeromission.io>
2025-11-04 09:07:50 +0000

    Refactor

--- quest1/part1.cpp
+++ quest1/part1.cpp
@@ -1,47 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <numeric>
-#include <ranges>
-#include <vector>
-#include <stdexcept>
-
-
-std::vector<std::string> split(std::string text, std::string sep)
-{
-    std::vector<std::string> list;
-    int start = 0;
-    auto end = text.find(sep, start + 1);
-    while (start < text.length())
-    {
-        end = text.find(sep, start + 1);
-        if (end == std::string::npos)
-            end = text.length();
-
-        list.push_back(text.substr(start, end - start));
-        start = end + 1;
-    }
-    return list;
-}
-
+#include "common.cpp"
 
 int main()
 {
-    std::ifstream file("input1.txt");
-    if (!file.is_open())
-    {
-        std::cerr << "Error: Could not open file." << std::endl;
-        return 0;
-    }
-
-    std::string wordsLine;
-    std::string temp;
-    std::string instructionsLine;
-    std::getline(file, wordsLine);
-    std::getline(file, temp);
-    std::getline(file, instructionsLine);
-
-    auto words = split(wordsLine, ",");
-    auto instructions = split(instructionsLine, ",");
+    auto lines = readLinesFromFile("input1.txt");
+    auto words = split(lines[0], ",");
+    auto instructions = split(lines[2], ",");
 
     auto wordIndex = 0;
     for (const auto &instruction : instructions)
--- quest1/part3.cpp
+++ quest1/part3.cpp
@@ -1,53 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <numeric>
-#include <ranges>
-#include <vector>
-#include <stdexcept>
-
-
-std::vector<std::string> split(std::string text, std::string sep)
-{
-    std::vector<std::string> list;
-    int start = 0;
-    auto end = text.find(sep, start + 1);
-    while (start < text.length())
-    {
-        end = text.find(sep, start + 1);
-        if (end == std::string::npos)
-            end = text.length();
-
-        list.push_back(text.substr(start, end - start));
-        start = end + 1;
-    }
-    return list;
-}
-
-
-inline int mod(int a, int b) {
-    int ret = a % b;
-    return ret >= 0 ? ret : ret + b;
-}
-
+#include "common.cpp"
 
 int main()
 {
-    std::ifstream file("input3.txt");
-    if (!file.is_open())
-    {
-        std::cerr << "Error: Could not open file." << std::endl;
-        return 0;
-    }
-
-    std::string wordsLine;
-    std::string temp;
-    std::string instructionsLine;
-    std::getline(file, wordsLine);
-    std::getline(file, temp);
-    std::getline(file, instructionsLine);
-
-    auto words = split(wordsLine, ",");
-    auto instructions = split(instructionsLine, ",");
+    auto lines = readLinesFromFile("input3.txt");
+    auto words = split(lines[0], ",");
+    auto instructions = split(lines[2], ",");
 
     for (const auto &instruction : instructions)
     {
