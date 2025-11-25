118f8f91549d025c81c1bb54036696d879715d4d
davidbetteridge <david.betteridge@zeromission.io>
2025-11-25 09:39:18 +0000

    bitset

--- quest14/part3.cpp
+++ quest14/part3.cpp
@@ -18,55 +18,59 @@ int main()
     // for (const auto &line : lines)
     //     tiles+=line;
 
-    const int size = 34 * 34;
-    static std::array<bool, 34*34> board = {0}; 
+    
+    static std::array<std::bitset<34>, 34> board1 = {0}; 
+    static std::array<std::bitset<34>, 34> board2 = {0}; 
 
     auto total = 0;
+    auto previousBoard = &board1;
+    auto nextBoard = &board2;
+
     for(auto round=0;round<1000000000;round++)
     {
-        static std::array<bool, 34*34> nextBoard = {0}; 
-
-        for(auto i=0;i<size;i++)
+        for(auto row=0;row<34;row++)
         {
-            auto x = i % 34;
-            auto y = (i - x) / 34;
+            for(auto column=0;column<34;column++)
+            {
 
-            auto activeDiagonals = 0;
-            for (const auto &diagonal : diagonals)
-            {
-                auto newX = x + diagonal.first;
-                auto newY = y + diagonal.second;
-                if (newX >= 0 && newY >= 0 && newX < 34 && newY < 34 )
+                // Count active diagonals 
+                auto activeDiagonals = 0;
+                for (const auto &diagonal : diagonals)
                 {
-                    auto index = (newY * 34) + newX;
-                    if (board[index])
-                        activeDiagonals++;                    
+                    auto newX = column + diagonal.first;
+                    auto newY = row + diagonal.second;
+                    if (newX >= 0 && newY >= 0 && newX < 34 && newY < 34 )
+                    {
+                        auto index = (newY * 34) + newX;
+                        if ((*previousBoard)[newY].test(newX))
+                            activeDiagonals++;                    
+                    }
                 }
-            }
-            if (board[i])
-            {
-                if (activeDiagonals % 2 == 1)
+
+                if ((*previousBoard)[row].test(column))
                 {
-                    nextBoard[i] = true;
-                    total++;
-                }
-                else
-                    nextBoard[i] = false;
-            }
-            else
-            {
-                if (activeDiagonals % 2 == 0)
-                {
-                    nextBoard[i] = true;
-                    total++;
+                    if (activeDiagonals % 2 == 1)
+                    {
+                        (*nextBoard)[row].set(column,true);
+                    }
+                    else
+                        (*nextBoard)[row].set(column,false);
                 }
                 else
-                    nextBoard[i] = false;
+                {
+                    if (activeDiagonals % 2 == 0)
+                    {
+                        (*nextBoard)[row].set(column,true);
+                    }
+                    else
+                        (*nextBoard)[row].set(column,false);
+                }
+
+
             }
         }
-
-        board = nextBoard;
-        
+        std::swap(previousBoard, nextBoard);
+       
     }
 
     std::cout << total << std::endl;
